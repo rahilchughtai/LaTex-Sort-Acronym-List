16be262b37509f16f8017910ee07e80e3dda0c5d
Rahil CHUGHTAI <rahil.chughtai@amadeus.com>
2023-06-13 16:52:30 +0200

    update for proper syntax

--- script.py
+++ script.py
@@ -1,36 +1,8 @@
-content = open('acro.txt', 'r').readlines()
-
-# Setup
-acros = {}
-currTrack = ''
-isOngoingAcro = False
-currKey = ''
-
-
-def getAcronymFull(line: str): return line[line.find(']')+2:-2]
-
-# Iterate Over Acronym Document
-for line in content:
-    if line.strip().startswith(r'\acro{'):
-        currKey = getAcronymFull(line)
-
-    if line.startswith(r'\begin{acronym}'):
-        isOngoingAcro = True
-
-    if line.startswith(r'\end{acronym}'):
-        isOngoingAcro = False
-        currTrack += line
-        acros[currKey] = currTrack
-        currTrack = ''
-
-    if(isOngoingAcro):
-        currTrack += line
-
-acroKeys = sorted([x for x in acros.keys()])
-
+acros = [l.strip() for l in open('acro.txt', 'r').readlines() if len(l.strip()) >0  ]
+def getAcronymFull(line: str): return line[line.find(']')+2:-1]
+acros.sort(key=lambda x:getAcronymFull(x))
 output = open("output.txt", "w")
-
-for key in acroKeys:
-    output.write(acros[key])
+for a in acros:
+    output.write(a)
     output.write('\n')
 output.close()
